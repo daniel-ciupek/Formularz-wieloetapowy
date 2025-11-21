a6dcaf8973773c4cec67b3904c1aa05cd53e58cf
daniel-ciupek <danter005@o2.pl>
2025-11-21 19:53:19 +0100

    kod JS zmiany kroku w formularzu

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 class MultiStepForm {
-    constructor() {
+  constructor() {}
 
-    }
+  init() {
+    this.form = document.querySelector("form");
+    this.steps = Array.from(document.querySelectorAll("form .step"));
+    this.nextBtn = document.querySelectorAll("form .next-btn");
+    this.prevBtn = document.querySelectorAll("form .previous-btn");
 
-    init() {
+    this.addListeners();
+  }
 
-        this.form = document.querySelector("form");
-        this.steps = Array.from(document.querySelectorAll("form .step"));
-        this.nextBtn = document.querySelectorAll("form .next-btn");
-        this.prevBtn = document.querySelectorAll("form .previous-btn");
-
-        this.addListeners();
-    }
-
-   addListeners() {
-    this.nextBtn.forEach(btn => {
-        btn.addEventListener("click", () => this.changeStep(1));
+  addListeners() {
+    this.nextBtn.forEach((btn) => {
+      btn.addEventListener("click", () => this.changeStep(1));
     });
 
-    this.prevBtn.forEach(btn => {
-        btn.addEventListener("click", () => this.changeStep(-1));
+    this.prevBtn.forEach((btn) => {
+      btn.addEventListener("click", () => this.changeStep(-1));
     });
 
     this.form.addEventListener("submit", this.submit);
+  }
+
+  changeStep = (v) => {
+    const activeStep = document.querySelector(".active");
+    let stepIndex = this.steps.indexOf(activeStep);
+    this.steps[stepIndex].classList.remove("active");
+    stepIndex += v;
+    if (v === 0) stepIndex = 0;
+
+    if (stepIndex < 0 || stepIndex >= this.steps.length) stepIndex = 0;
+    this.steps[stepIndex].classList.add("active");
+  };
+  resetSteps = () => {
+    this.changeStep(0);
+  };
+
+  submit = (e) => {
+    e.preventDefault();
+    this.form.reset();
+    this.resetSteps();
+  };
 }
-
-
-
-    changeStep = (v) => {
-
-    }
-    resetSteps = () => {
-        this.changeStep(0);
-    }
-
-    submit = (e) => {
-       e.preventDefault();
-       this.form.reset(); 
-       this.resetSteps();
-    }
-}
-
 
 const multiForm = new MultiStepForm();
 multiForm.init();
-
-
